d47b1f3ee29ce306400819f8585a14a51f7ebf11
mashpolo <luluo.polo1@gmail.com>
2018-08-22 11:13:44 +0800

    add convert array to tree

---
+++ convert_array_tree/ans.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@desc:   
+@author: luluo
+@date:   2018-8-22
+
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
+
+
