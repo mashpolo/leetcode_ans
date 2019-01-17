e52e8580bb1eab1baa23c85a639e3a9b27e1546b
mashpolo <luluo.polo1@gmail.com>
2019-01-17 10:16:23 +0800

    add leetcode 743

---
+++ 700/leetcode743/ans.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@desc:
+@author: Luo.lu
+@date:   2019-01-17
+
+"""
+import collections
+
+
+class Solution:
+    def networkDelayTime(self, times, N, K):
+        """
+        :type times: List[List[int]]
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        K -= 1
+        nodes = collections.defaultdict(list)
+        for u, v, w in times:
+            nodes[u - 1].append((v - 1, w))
+        dist = [float('inf')] * N
+        dist[K] = 0
+        done = set()
+        for _ in range(N):
+            smallest = min((d, i) for (i, d) in enumerate(dist) if i not in done)[1]
+            for v, w in nodes[smallest]:
+                if v not in done and dist[smallest] + w < dist[v]:
+                    dist[v] = dist[smallest] + w
+            done.add(smallest)
+        return -1 if float('inf') in dist else max(dist)
