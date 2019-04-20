8c2a9d9b4c602989ff7c369a2f36eca971a829ea
mashpolo <luluo.polo1@gmail.com>
2019-04-20 11:47:24 +0800

    add leetcode 32

---
+++ 100/leetcode32/ans.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@desc:
+@author: Luo.lu
+@date:   2019-04-20
+
+"""
+
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = []
+        maxLen = 0
+        last = -1
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                if not stack:
+                    last = i
+                else:
+                    stack.pop()
+                    if not stack:
+                        maxLen = max(maxLen,i - last)
+                    else:
+                        maxLen = max(maxLen,i - stack[-1])
+
+        return maxLen
+
+
+if __name__ == '__main__':
+    A = Solution()
+    print(A.longestValidParentheses("()(())"))
