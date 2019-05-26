02e6885f03dcd44bb2631b884d7e457dd4c9c969
mashpolo <luluo.polo1@gmail.com>
2019-05-26 11:55:16 +0800

    Create ans.py

---
+++ 100/leetcode80/ans.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        for e in nums:
+            if i < 2 or e != nums[i - 2]:
+                nums[i] = e
+                i += 1
+
+        return i
