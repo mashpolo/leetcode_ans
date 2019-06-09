384ed8b3326075ae0f0112aadb48a199e859caf3
mashpolo <luluo.polo1@gmail.com>
2019-06-09 22:47:20 +0800

    add leetcode 96

---
+++ 100/leetcode96/ans.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+@desc:
+@author: Luo.lu
+@date:   2019-06-09
+
+"""
+
+
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        return dp[n]
